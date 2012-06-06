1f3291f670900a3bbea27d2745de8f22063abc10
Jurgens du Toit <jrgns@jrgns.net>
2012-06-06 08:00:35 +0200

    Extended documentation, added delete function, fixed exceptions

--- Bindings/GdataBinding.php
+++ Bindings/GdataBinding.php
@@ -1,21 +1,73 @@
 <?php
+/**
+ * File defining GdataBinding
+ *
+ * PHP Version 5.3
+ *
+ * @category   Backend
+ * @package    Base
+ * @subpackage Bindings
+ * @author     J Jurgens du Toit <jrgns@backend-php.net>
+ * @copyright  2011 - 2012 Jade IT (cc)
+ * @license    http://www.opensource.org/licenses/mit-license.php MIT License
+ * @link       http://backend-php.net
+ */
 namespace Backend\Base\Bindings;
-set_include_path(get_include_path() . PATH_SEPARATOR . '/usr/share/php/libzend-framework-php');
 require_once 'Zend/Loader.php';
 \Zend_Loader::loadClass('Zend_Gdata_ClientLogin');
 \Zend_Loader::loadClass('Zend_Gdata_Gapps');
 use Backend\Core\Utilities\ServiceLocator;
 use Backend\Core\Exceptions\BackendException;
+/**
+ * Binding for GData connections.
+ *
+ * This class relies on the Zend library, so you need to ensure that it's on the
+ * scripts include path. You can install Zend through PEAR:
+ *
+ *     pear channel-discover zend.googlecode.com/svn
+ *     pear install --alldeps zend/zend
+ *
+ * There's currently a known compatability issue. See http://framework.zend.com/issues/browse/ZF-11959
+ * for more detail.
+ *
+ * @category   Backend
+ * @package    Base
+ * @subpackage Bindings
+ * @author     J Jurgens du Toit <jrgns@jrgns.net>
+ * @license    http://www.opensource.org/licenses/mit-license.php MIT License
+ * @link       http://backend-php.net
+ * @todo This is a rudimentary implementation of the PDOBinding. It can be improved a lot.
+ */
 class GdataBinding extends Binding
 {
+    /**
+     * API Key used to access the GApps API. Unused for now.
+     * 
+     * @var string
+     */
     protected $key;
 
+    /**
+     * API Secret to access the GApps API. Unused for now.
+     *
+     * @var string
+     */
     protected $secret;
 
+    /**
+     * The client used to access the GApps API.
+     *
+     * @Zend_Gdata_Gapps
+     */
     protected $client;
 
     /**
      * The constructor for the object.
+     *
+     * The settings array must contain the name of the entity to bind to, the name
+     * of the connection to use, as well as the username and password for the admin
+     * user that will be used to access the API. You should also provide a domain if
+     * it's different that that of the admin user's password.
      *
      * @param array $settings The settings for the Service Connection
      */
@@ -29,7 +81,12 @@ class GdataBinding extends Binding
             throw new BackendException('Missing Google Admin User Credentials');
         }
         if (empty($settings['domain'])) {
-            throw new BackendException('Missing Google Domain');
+            $domain = explode($settings['username']);
+            if (empty($domain[1])) {
+                throw new BackendException('Missing Google Domain');
+            } else {
+                $settings['domain'] = $domain[1];
+            }
         }
 
         // Get the Client
@@ -58,23 +115,6 @@ class GdataBinding extends Binding
         return $result;
     }
 
-    protected function mapObjectToModel(\Zend_Gdata_Gapps_UserEntry $object)
-    {
-        $name  = $object->getName();
-        $login = $object->getLogin();
-        $properties  = array(
-            'given_name'  => $name->getGivenName(),
-            'family_name' => $name->getFamilyName(),
-            'username'    => $login->getUsername(),
-            'password'    => $login->getPassword(),
-            'admin'       => $login->getAdmin(),
-            'active'      => !(boolean)$login->getSuspended(),
-        );
-        $object = new $this->className;
-        $object->populate($properties);
-        return $object;
-    }
-
     /**
      * Create an instance on the source, and return the instance.
      *
@@ -93,8 +133,7 @@ class GdataBinding extends Binding
                 $data['password']
             );
         } catch (\Zend_Gdata_Gapps_ServiceException $e) {
-            //TODO Wrap this in an exception understood by Backend
-            return false;
+            throw new \Backend\Core\Exceptions\BackendException('Cannot created requested resource', null, $e);
         }
 
         return $this->mapObjectToModel($result);
@@ -114,8 +153,7 @@ class GdataBinding extends Binding
         try {
             $result = $this->client->retrieveUser($identifier);
         } catch (\Exception $e) {
-            //TODO Wrap this in an exception understood by Backend
-            return false;
+            throw new \Backend\Core\Exceptions\BackendException('Cannot retrieve requested information', null, $e);
         }
         return $this->mapObjectToModel($result);
     }
@@ -160,5 +198,62 @@ class GdataBinding extends Binding
      */
     public function delete(\Backend\Core\Interfaces\ModelInterface &$model)
     {
+        try {
+            $result = $this->client->deleteUser($identifier);
+        } catch (\Exception $e) {
+            throw new \Backend\Core\Exceptions\BackendException('Cannot delete the specified resource', null, $e);
+        }
+        return true;
+    }
+
+    /**
+     * Map Zend_Gdata_Gapps_UserEntry objects to Backend Models
+     *
+     * @param Zend_Gdata_Gapps_UserEntry $object The object to map.
+     *
+     * @return \Backend\Core\Interfaces\ModelInterface The model.
+     */
+    protected function mapObjectToModel(\Zend_Gdata_Gapps_UserEntry $object)
+    {
+        $name  = $object->getName();
+        $login = $object->getLogin();
+        $properties  = array(
+            'given_name'  => $name->getGivenName(),
+            'family_name' => $name->getFamilyName(),
+            'username'    => $login->getUsername(),
+            'password'    => $login->getPassword(),
+            'admin'       => $login->getAdmin(),
+            'active'      => !(boolean)$login->getSuspended(),
+        );
+        $object = new $this->className;
+        $object->populate($properties);
+        return $object;
+    }
+
+    /**
+     * Map a Backend Model to a Zend_Gdata_Gapps_User Entry.
+     *
+     * The model should provide the following functions for this function to
+     * work correctly:
+     *
+     * * getUsername()
+     * * getPassword()
+     * * getGivenName()
+     * * getFamilyName()
+     *
+     * @param \Backend\Core\Interfaces\ModelInterface The model to map.
+     *
+     * @return Zend_Gdata_Gapps_UserEntry The object
+     */
+    protected function mapModelToObject(\Backend\Core\Interfaces\ModelInterface $model)
+    {
+        $user = $gdata->newUserEntry();
+        $user->login = $gdata->newLogin();
+        $user->login->username = $model->getUsername();
+        $user->login->password = $model->getPassword();
+        $user->name = $gdata->newName();
+        $user->name->givenName = $model->getGivenName();
+        $user->name->familyName = $model->getFamilyName();
+        return $user;
     }
 }
