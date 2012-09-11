38c375bb4590499b2a667bff077974c36a88c02f
Jurgens du Toit <jrgns@jrgns.net>
2012-09-11 09:07:21 +0200

    Fixed packages on Tests

--- Tests/ApplicationTest.php
+++ Tests/ApplicationTest.php
@@ -5,7 +5,7 @@
  * PHP Version 5.3
  *
  * @category  Backend
- * @package   CoreTests
+ * @package   BaseTests
  * @author    J Jurgens du Toit <jrgns@backend-php.net>
  * @copyright 2011 - 2012 Jade IT (cc)
  * @license   http://www.opensource.org/licenses/mit-license.php MIT License
@@ -19,7 +19,7 @@ use Backend\Core\Utilities\DependencyInjectionContainer;
  * Class to test the \Backend\Base\Application class
  *
  * @category Backend
- * @package  CoreTests
+ * @package  BaseTests
  * @author   J Jurgens du Toit <jrgns@backend-php.net>
  * @license  http://www.opensource.org/licenses/mit-license.php MIT License
  * @link     http://backend-php.net
--- Tests/ModelTest.php
+++ Tests/ModelTest.php
@@ -5,7 +5,7 @@
  * PHP Version 5.3
  *
  * @category  Backend
- * @package   CoreTests
+ * @package   BaseTests
  * @author    J Jurgens du Toit <jrgns@backend-php.net>
  * @copyright 2011 - 2012 Jade IT (cc)
  * @license   http://www.opensource.org/licenses/mit-license.php MIT License
@@ -18,7 +18,7 @@ require_once __DIR__ . '/auxiliary/TestModel.php';
  * Class to test the \Backend\Base\Model class
  *
  * @category Backend
- * @package  CoreTests
+ * @package  BaseTests
  * @author   J Jurgens du Toit <jrgns@backend-php.net>
  * @license  http://www.opensource.org/licenses/mit-license.php MIT License
  * @link     http://backend-php.net
